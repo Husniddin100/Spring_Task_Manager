fa3702e42f2dec562a9ae7499fde5fe976ca0507
Husniddin <uchanal10@gmail.com>
2024-03-07 15:23:16 +0500

    test

--- src/main/java/com/example/Spring_TaskManager/controller/TaskController.java
+++ src/main/java/com/example/Spring_TaskManager/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,22 +19,26 @@ public class TaskController {
     public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO dto) {
         return ResponseEntity.ok(taskService.createTask(dto));
     }
+
     @GetMapping("/task_list")
     public ResponseEntity<List<TaskDTO>> getList() {
         List<TaskDTO> list = taskService.getList();
         return ResponseEntity.ok(list);
     }
+
     @GetMapping("/completed_task_list")
     public ResponseEntity<List<TaskDTO>> getCompletedTaskList() {
         List<TaskDTO> list = taskService.getCompletedTaskList();
         return ResponseEntity.ok(list);
     }
+
     @PutMapping("/completedTask/{id}")
-    public ResponseEntity<Boolean>completedTask(@PathVariable Integer id){
+    public ResponseEntity<Boolean> completedTask(@PathVariable Integer id) {
         return ResponseEntity.ok(taskService.completedTask(id));
     }
+
     @GetMapping("/getById/{id}")
-    public ResponseEntity<TaskDTO> getById(@PathVariable Integer id){
+    public ResponseEntity<TaskDTO> getById(@PathVariable Integer id) {
         return ResponseEntity.ok(taskService.getById(id));
     }
 }
--- src/main/java/com/example/Spring_TaskManager/repository/TaskRepository.java
+++ src/main/java/com/example/Spring_TaskManager/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.example.Spring_TaskManager.repository;
 
 import com.example.Spring_TaskManager.entity.TaskEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TaskRepository extends CrudRepository<TaskEntity,Integer> {
+import java.time.LocalDateTime;
+
+public interface TaskRepository extends CrudRepository<TaskEntity, Integer> {
+    @Query("from TaskEntity where  created_date=?1")
+    Iterable<TaskEntity> getAll(LocalDateTime date);
 }
